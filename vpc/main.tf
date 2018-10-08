2455e37cbffbb86ac56b995933637599cd7b272f
devops <thierry.ciboire@eulerhermes.com>
2018-10-08 15:12:02 +0200

    SUBNET CREATION

--- vpc/main.tf
+++ vpc/main.tf
@@ -11,3 +11,12 @@ resource "aws_vpc" "Myvpc" {
     Name = "VPC_DS2SI"
   }
 }
+
+resource "aws_subnet" "MySubnet" {
+  vpc_id     = "${aws_vpc.Myvpc.id}"
+  cidr_block = "172.23.1.0/24"
+
+  tags {
+    Name = "Subnet_D2SI"
+  }
+}
